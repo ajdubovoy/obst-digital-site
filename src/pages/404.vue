520678e7f13ef2112bb6d727632217a70b438a47
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-08-22 16:23:28 +0200

    set up global css

---
+++ src/pages/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    
+    <h1>404 - Not Found</h1>
+   
+    <p>
+      Sorry! We're working on it, but that fruit hasn't been planted yet...
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404'
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,16 +3,12 @@
     
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
+   
+    <h4>Design & Development Studio</h4>
+    <h1>We help you build brands, websites, and digital products.</h1>
    
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      We are a collective of creative professionals brought together by our passion for technology and desire to build better digital products.
     </p>
 
   </Layout>
@@ -21,13 +17,10 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Home'
   }
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
+<style lang="scss">
 </style>
