1f920c9b03d55fecc3402404860361335b23d1e6
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-08-26 16:25:44 +0200

    display proper svgs

--- gridsome.config.js
+++ gridsome.config.js
@@ -32,5 +32,12 @@ module.exports = {
     types.forEach(type => {
       addStyleResource(config.module.rule('scss').oneOf(type))
     })
+
+    // SVG
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
 	}
 }
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,29 @@
 <template>
   <nav class="nav">
     <g-link to="/">
-      <g-image v-if=dark src="~/assets/images/logo_dark.svg" alt="Obst Digital" />
-      <g-image v-if=!dark src="~/assets/images/logo_light.svg" alt="Obst Digital" />
+      <LogoDark v-if=dark alt="Obst Digital" class="logo" />
+      <LogoLight v-if=!dark alt="Obst Digital" class="logo" />
     </g-link>
   </nav>
 </template>
 
 <script>
+import LogoDark from '~/assets/images/logo_dark.svg';
+import LogoLight from '~/assets/images/logo_light.svg';
 export default {
   name: 'Card',
   props: {
     dark: Boolean
+  },
+  components: {
+    LogoDark,
+    LogoLight
   }
 }
 </script>
+
+<style scoped lang="scss">
+.logo{
+  width: 10.5rem;
+}
+</style>
